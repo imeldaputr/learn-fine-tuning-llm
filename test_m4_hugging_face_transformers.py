547d10a7ec8e07956327f0afe89cdef439e05e95
imeldaputr <imel.kein.email@gmail.com>
2025-11-26 16:48:59 +0100

    update file test hf to test another model

--- test_m4_hugging_face_transformers.py
+++ test_m4_hugging_face_transformers.py
@@ -12,18 +12,32 @@ print(f"Loading model {model_name}...")
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     dtype=torch.float16,
-    device_map="mps", # Metal Performance Shaders
-    low_cpu_mem_usage=True
+    device_map="auto", # mps: Metal Performance Shaders
+    trust_remote_code=True
+    #low_cpu_mem_usage=True
 )
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
-prompt = "def calculate_sum(numbers):\n "
-inputs = tokenizer(prompt, return_tensors="pt").to("mps")
+messages = [
+    {
+        "role": "user",
+        "content": "Write a quick sort algorithm in Python."
+    }
+]
 
-print("Generating...")
-outputs = model.generate(
-    **inputs,
-    max_length=100
-)
-print(tokenizer.decode(outputs[0]))
+#prompt = "def calculate_sum(numbers):\n "
+
+inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(model.device)
+
+outputs = model.generate(inputs, max_new_tokens=512, do_sample=False, pad_token_id=tokenizer.pad_token_id)
+
+result = tokenizer.decode(outputs[0][len(inputs[0]):], skip_special_tokens=True)
+print(result)
+
+# print("Generating...")
+# outputs = model.generate(
+#     **inputs,
+#     max_length=100
+# )
+# print(tokenizer.decode(outputs[0]))
